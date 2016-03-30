6a8a91c54887a78a3ea2dc366788ee392858392b
Ahmad Ben Mrad <batikhsouri@gmail.com>
2016-03-30 15:42:24 +0200

    Adding syntax check, and adding libsodium-wrappers as node dependency

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt){
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jsvalidate: {
+			options: {
+				globals: {},
+				esprimaOptions: {},
+				verbose: true
+			},
+			src: ['lawncipher.js'],
+			tests: ['test.js']
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-jsvalidate');
+
+	grunt.registerTask('default', ['jsvalidate']);
+
+};
