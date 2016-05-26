b2cc377d596cb2e646097443c4a10a7a541374d3
Ahmad Ben Mrad <batikhsouri@gmail.com>
2016-05-26 14:22:22 +0200

    Clearing previous index, and recreating the tests/test_index directory

--- test_all.sh
+++ test_all.sh
@@ -5,6 +5,9 @@ set -e
 if [ -d "./test_db" ]; then
 	rm -r ./test_db
 fi
+if [ -d "./tests/test_index" ]; then
+	rm -r ./tests/test_index
+fi
 
 echo "-----------------------------------"
 echo "Unit testing Lawncipher's internals"
@@ -21,6 +24,7 @@ node tests/pearsonrange.js
 echo "Pearson-based B+ trees"
 node tests/pearsonbplustree.js
 echo "Splitted indexes"
+mkdir -p tests/test_index
 node tests/splitindex.js
 
 echo ""
